2c5dc8043ab711682c7d72970a30477ade7fbe0d
James Griess <11860216+JamesGriess97@users.noreply.github.com>
2017-10-24 14:12:19 -0500

    Movement works, currently using a character controller

--- Code/Assets/Scripts/PlayerController.cs
+++ Code/Assets/Scripts/PlayerController.cs
@@ -1,28 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    Camera cam;
+    public Transform playerCam, character, centerPoint;
 
-	// Use this for initialization
-	void Start () {
-        cam = Camera.main;	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetMouseButton(0)) {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+    private float mouseX, mouseY;
+    public float mouseSensitivity = 10f;
+    public float mouseYPosition = 1f;
 
-            if (Physics.Raycast(ray, out hit)) {
-                Debug.Log("we hit" + hit.collider.name);
-                // Move our playaer to what we hit
+    private float moveFB, moveLR;
+    public float moveSpeed = 2f;
 
-                // Stop focusing any objects
-            }
+    public float rotationSpeed = 5f;
+
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        mouseX += Input.GetAxis("Mouse X");
+        mouseY -= Input.GetAxis("Mouse Y");
+        mouseY = Mathf.Clamp(mouseY, 10, 60f);
+
+        playerCam.LookAt(centerPoint);
+        centerPoint.localRotation = Quaternion.Euler(mouseY, mouseX, 0);
+
+        moveFB = Input.GetAxis("Vertical") * moveSpeed;
+        moveLR = Input.GetAxis("Horizontal") * moveSpeed;
+
+        Vector3 movement = new Vector3(moveLR, 0, moveFB);
+        movement = character.rotation * movement;
+        character.GetComponent<CharacterController>().Move(movement * Time.deltaTime);
+        centerPoint.position = new Vector3(character.position.x, character.position.y + mouseYPosition, character.position.z);
+
+        if (Input.GetAxis("Vertical") > 0 | Input.GetAxis("Vertical") < 0) {
+
+            Quaternion turnAngle = Quaternion.Euler(0, centerPoint.eulerAngles.y, 0);
+
+            character.rotation = Quaternion.Slerp(character.rotation, turnAngle, Time.deltaTime * rotationSpeed);
+
         }
-	}
+
+    }
 }
