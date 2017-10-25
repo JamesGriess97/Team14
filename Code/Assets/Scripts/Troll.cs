0d42234f4029b96d98262a7040fad05c7ec268c7
Ja'lon <jclark96@sbcglobal.net>
2017-10-25 17:09:19 -0500

    Start to AI
    Start to Ai code - still need to figure out how attack is going to work and see if the auto movement actually works

---
+++ Code/Assets/Scripts/Troll.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Troll : MonoBehaviour {
+	
+	public GameObject player;
+	
+	private Vector3 distance;
+	
+
+	// Use this for initialization
+	void Start () {
+		distance = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if the player is close enough, attack else move towards the player
+		
+		if(distance < 3){
+			
+		}
+		else{
+			transform.position -= transform.position;
+		}
+		
+	}
+}
