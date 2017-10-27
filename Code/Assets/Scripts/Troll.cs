ae5309b81a3624aec977894ace55bdc4ff440e23
Ja'lon <jclark96@sbcglobal.net>
2017-10-27 17:13:00 -0500

    AI Continuation
    platform like movement attempt

--- Code/Assets/Scripts/Troll.cs
+++ Code/Assets/Scripts/Troll.cs
@@ -6,26 +6,44 @@ public class Troll : MonoBehaviour {
 
 	
 	public GameObject player;
-	
+	public Transform troll;
 	private Vector3 distance;
+	Animator anim;
+    public float gravity = 9.8f;
+    private float vSpeed = 0f;
+	public float moveSpeed = 1f;
+    public float rotationSpeed = 5f;
+	private float moveFB, moveLR;
 	
-
 	// Use this for initialization
 	void Start () {
-		distance = transform.position - player.transform.position;
+		moveFB = 0;
+		moveLR = 0;
 	    int trollHealth = 5;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//if the player is close enough, attack else move towards the player
-
-		/*
-		if(distance < 3) {
-			
-		} else{
-			transform.position -= transform.position;
+		distance = transform.position - player.transform.position;
+		//move on a set path
+		if(moveLR < 10){
+			moveLR = moveLR++;
 		}
-		*/
+		else{
+			moveLR = moveLR--;
+		}
+		
+		Vector3 movement = new Vector3(moveLR, 0, moveFB);
+		troll.GetComponent<CharacterController>().Move(movement * Time.deltaTime);	
+	
+		//if the player is close enough launch an attack
+		if(distance.magnitude < 3) {
+			//anim.SetBool ("Attack", true);
+		} 
+		else{
+			//anim.SetBool ("Attack", false);
+		}
+		
+		
 	}
 }
