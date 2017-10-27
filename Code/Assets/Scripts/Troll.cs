88714df93ec26fe425d68f59a18264c04333671c
James Griess <11860216+JamesGriess97@users.noreply.github.com>
2017-10-27 18:13:35 -0500

    enemmies can now move

--- Code/Assets/Scripts/Troll.cs
+++ Code/Assets/Scripts/Troll.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class Troll : MonoBehaviour {
 
 	
-	public GameObject player;
+	public Transform player;
 	public Transform troll;
+
 	private Vector3 distance;
 	Animator anim;
     public float gravity = 9.8f;
@@ -14,36 +15,35 @@ public class Troll : MonoBehaviour {
 	public float moveSpeed = 1f;
     public float rotationSpeed = 5f;
 	private float moveFB, moveLR;
-	
+    private bool foo = false;
+
+    private Vector3 posOrig;
 	// Use this for initialization
 	void Start () {
 		moveFB = 0;
 		moveLR = 0;
 	    int trollHealth = 5;
+        posOrig = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		distance = transform.position - player.transform.position;
-		//move on a set path
-		if(moveLR < 10){
-			moveLR = moveLR++;
-		}
-		else{
-			moveLR = moveLR--;
-		}
-		
-		Vector3 movement = new Vector3(moveLR, 0, moveFB);
-		troll.GetComponent<CharacterController>().Move(movement * Time.deltaTime);	
-	
-		//if the player is close enough launch an attack
-		if(distance.magnitude < 3) {
-			//anim.SetBool ("Attack", true);
-		} 
-		else{
-			//anim.SetBool ("Attack", false);
-		}
-		
-		
-	}
+
+		Vector3 movement = new Vector3(transform.position.x * moveSpeed, 0, transform.position.y * moveSpeed);
+        if(Vector3.Distance(transform.position, posOrig) > 20 && !foo) {
+            moveSpeed = moveSpeed * -1;
+            foo = true;
+        } else if (Vector3.Distance(transform.position, posOrig) < 5 && foo) {
+            moveSpeed = moveSpeed * -1;
+            foo = false;
+        }
+
+        //Debug.Log("distance: " + Vector3.Distance(transform.position, posOrig));
+
+
+        troll.GetComponent<CharacterController>().Move(movement * Time.deltaTime);
+ 
+
+       
+    }
 }
