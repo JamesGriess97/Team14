c78b5372078441c6dee35b55945932ecb00598a7
James Griess <11860216+JamesGriess97@users.noreply.github.com>
2017-10-27 10:13:27 -0500

    added destructable barrel

---
+++ Code/Assets/collisionDetector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionDetector : MonoBehaviour {
+
+    private int health = 3;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("Health = " + health);
+        health -= 1;
+        if (health == 0) {
+            Destroy(gameObject);
+        }
+    }
+    
+}
