c71c3caa2e456e8fea11ac860b33e1f5086ee8c0
Ja'lon <jclark96@sbcglobal.net>
2017-11-28 15:46:33 -0600

    AI start/Troll animation fix
    I fixed the troll animation, modified the file system to consolidate the troll controls and began the AI

---
+++ Code/Assets/Scripts/AIProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIProcessor : MonoBehaviour {
+	Animator anim;
+	
+	public Transform player;
+	public Transform Troll;
+	private float success;
+	private float fail;
+	private float playerSuccess;
+	private float playerFail;
+	private float combo1Count;
+	private float combo2Count;
+	private bool playerInRange;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Code/Assets/Scripts/Troll.cs
+++ Code/Assets/Scripts/Troll.cs
@@ -2,58 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class Troll : MonoBehaviour {
     public int health = 5;
 	public Slider healthSlider;
-
-
+	PlayerHealth playerHealth;
     public Transform player;
 	public Transform troll;
-
 	private Vector3 distance;
 	Animator anim;
     public float gravity = 9.8f;
     private float vSpeed = 0f;
-	public float moveSpeed = 1f;
+	public float moveSpeed = .2f;
     public float rotationSpeed = 5f;
-	private float moveFB, moveLR;
-    private bool foo = false;
-
+	UnityEngine.AI.NavMeshAgent nav;
 	private float playerAttackTime = 0.5f;
 	float timer =0f;
     private Vector3 posOrig;
+	bool aggressive;
+	bool distanceAttack;
+	AIProcessor brain;
+	
 	// Use this for initialization
 	void Start () {
-		moveFB = 0;
-		moveLR = 0;
-	    //int trollHealth = 5;
-        posOrig = transform.position;
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		nav = GetComponent <UnityEngine.AI.NavMeshAgent>();
+		anim = GetComponentInChildren<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 		timer += Time.deltaTime;
-		Vector3 movement = new Vector3(transform.position.x * moveSpeed, 0, transform.position.y * moveSpeed);
-        if(Vector3.Distance(transform.position, posOrig) > 20 && !foo) {
-            moveSpeed = moveSpeed * -1;
-            foo = true;
-        } else if (Vector3.Distance(transform.position, posOrig) < 5 && foo) {
-            moveSpeed = moveSpeed * -1;
-            foo = false;
-        }
-
-        //Debug.Log("distance: " + Vector3.Distance(transform.position, posOrig));
+		moveTroll();
 
 
-        troll.GetComponent<CharacterController>().Move(movement * Time.deltaTime);
+
+
+      
 
 
 
     }
 
-    void OnTriggerStay(Collider other) {
+/*     void OnTriggerStay(Collider other) {
 		
         //Debug.Log("collision");
 		if (Input.GetMouseButtonDown(0)&& timer >= playerAttackTime) {
@@ -67,6 +59,23 @@ public class Troll : MonoBehaviour {
         if (health == 0) {
             Destroy(gameObject);
         }
-    }
+    } */
+	
+	//controls troll movement procedures
+	void moveTroll(){
+		anim.SetBool("Walk",true);
+		if(aggressive)
+		nav.SetDestination (player.position);
+		
+	}
+	
+	//force troll stand to stand still
+	void idleTroll(){
+		anim.SetBool("Walk", false);
+	}
+	//troll attack procedures
+	void attack(){
+		anim.SetBool("Walk", false);
+	}
 
 }
