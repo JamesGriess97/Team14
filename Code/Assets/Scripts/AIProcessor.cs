054484ed1aea4b75f14b233139e6e56980dfa52d
Ja'lon <jclark96@sbcglobal.net>
2017-11-30 16:31:39 -0600

    AI framwork
    Began AI_Framework

--- Code/Assets/Scripts/AIProcessor.cs
+++ Code/Assets/Scripts/AIProcessor.cs
@@ -15,6 +15,14 @@ public class AIProcessor : MonoBehaviour {
 	private float combo2Count;
 	private bool playerInRange;
 
+	 // combo vars
+    int noOfClicks = 0;
+    //Time when last button was clicked
+    float startTime = 0;
+    float totalTime = 0;
+    float lastClickedTime = 0;
+    //Delay between clicks for which clicks will be considered as combo
+    public float maxComboDelay = .3f;
 	// Use this for initialization
 	void Start () {
 		
@@ -22,6 +30,68 @@ public class AIProcessor : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		int combo = comboController();
+		if( combo == 1 ){
+			
+		}
+		else if(combo == 2){
+			
+		}
+		else{
+			if(Input.GetKeyDown("space") || Input.GetButtonDown("X360_A")){
+				
+			}
+			else if(Input.GetAxis("Vertical") || Input.GetAxis("Horizontal")){
+				
+			}
+			else{
+				
+			}
+		}
+	}
+	
+	 int comboController() {
+        // attack animation combos
+        // this function could probably use some work
+
+        if (totalTime == 0 && lastClickedTime == 0) {
+            startTime = Time.time;
+        }
+
+        totalTime = Time.time - startTime;
+
+        if (Input.GetMouseButtonDown(0) || Input.GetButtonDown("X360_B")) {
+            lastClickedTime = Time.time;
+            noOfClicks++;
+            noOfClicks = Mathf.Clamp(noOfClicks, 0, 2);
+        }
+
+        if(totalTime >= maxComboDelay) {
+            Debug.Log("no clicks: " + noOfClicks);
+            if (noOfClicks == 1) {
+                noOfClicks = 0;
+                return 1;
+            } else if (noOfClicks == 2) {
+                noOfClicks = 0;
+                return 2;
+            }
+            noOfClicks = 0;
+            lastClickedTime = 0;
+            totalTime = 0;
+            startTime = 0;
+        }
+        return 0;
+    }
+	
+	bool inRange(){
+		
+	}
+	//
+	bool shouldAttack(){
+		
+	}
+	//which attack should be used against the player
+	String whichAttack(){
 		
 	}
 }
