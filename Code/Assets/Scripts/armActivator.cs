2b74f2dab7da7222d1a021b22f7f87a83b6334df
James Griess <11860216+JamesGriess97@users.noreply.github.com>
2017-12-01 15:17:40 -0600

    added collider to foot

--- Code/Assets/Scripts/armActivator.cs
+++ Code/Assets/Scripts/armActivator.cs
@@ -8,25 +8,30 @@ public class armActivator : MonoBehaviour {
     Collider footCollider;
     
     public GameObject arm;
+    public GameObject foot;
 
     // Use this for initialization
     void Start () {
-        //Fetch the GameObject's Collider (make sure they have a Collider component)
         armCollider = arm.GetComponent<Collider>();
+        footCollider = foot.GetComponent<Collider>();
+
         //Here the GameObject's Collider is not a trigger
         armCollider.isTrigger = false;
+        footCollider.isTrigger = false;
         //Output whether the Collider is a trigger type Collider or not
         Debug.Log("Trigger On : " + armCollider.isTrigger);
     }
 
-    void activateTrigger() {
-        //GameObject's Collider is now a trigger Collider when the GameObject is clicked. It now acts as a trigger
+    void armTrigger() {
         armCollider.isTrigger = true;
-        //Output to console the GameObject’s trigger state
-        Debug.Log("Trigger On : " + armCollider.isTrigger);
+    }
+
+    void footTrigger() {
+        footCollider.isTrigger = true;
     }
 
     void disableTrigger() {
         armCollider.isTrigger = false;
+        footCollider.isTrigger = false;
     }
 }
